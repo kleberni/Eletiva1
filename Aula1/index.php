0d6928054bccffaa1c55c3fb7369a07d28e1437c
kleberni <98528801+kleberni@users.noreply.github.com>
2024-08-20 19:32:16 -0300

    Ex1
    Primeiro aprendizado de PHP

---
+++ Aula1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hoje é dia <?php echo date('d/m/y'); ?></h1>
+    <form action="Aula1/resposta.php" method='POST'>
+        <input type="text" name="valor" />
+        <button type="submit">Enviar </button>
+    </form>
+</body>
+</html>
