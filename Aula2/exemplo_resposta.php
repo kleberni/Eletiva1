5e91c8c031fcf696f63352b9f0da5bf71613f2d9
kleberni <kleber.ni@gmail.com>
2024-08-27 20:28:59 -0300

    Aula 2 exemplo 1 e 2
    exemplos

---
+++ Aula2/exemplo_resposta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta</title>
+</head>
+<body>
+    <?php
+        if($_SERVER['REQUEST_METHOD'] == "POST") 
+        {
+            try {
+                $valor = (int) $_POST['valor'] ?? 0;
+                if ($valor > 0)
+                {
+                    echo "Valor positivo";
+                    
+                } elseif
+                {
+                    echo "valor Negativo";
+                } else
+                {
+                    echo "Valor informado é igual a zero"
+                }
+            } catch(Exception $e) {
+                echo "Erro: $e";
+            }
+        }
+    ?>
+</body>
+</html>
