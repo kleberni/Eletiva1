89cc4f63a58d87c2f986159db62c2646171085cf
kleberni <98528801+kleberni@users.noreply.github.com>
2024-09-22 14:33:29 +0000

    Exercicio6

--- Lista4/exer6.php
+++ Lista4/exer6.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifica Palavras</title>
+    <title>Arredondar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container">
-    <h1>Raiz Quadrada</h1>
-    <form action="exer5resp.php" method="POST">
+    <h1>Arredondar</h1>
+    <form action="exer6resp.php" method="POST">
         <div class="row">
             <div class="col">
                 <label for="numero" class="form-label">Número:</label>
-                <input type="number" class="form-control" id="numero" name="numero" required>
+                <input type="number" step="any" class="form-control" id="numero" name="numero" required>
             </div>
         </div>
         <button type="submit" class="btn btn-primary mt-3">Verificar</button>
--- Lista4/exer6resp.php
+++ Lista4/exer6resp.php
@@ -3,21 +3,21 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Arredondamento</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Raiz Quadrado</h1>
+    <h1>Arredondamento</h1>
     <?php
-      function RaizQuadrada($numero){
-        return sqrt($numero);
+      function arredondarNumero(float $numero): float {
+        return round($numero);
       }
 
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $numero = $_POST['numero'];
-        $Result_Raiz = RaizQuadrada($numero);
+        $numero = (float) $_POST['numero'];
+        $numeroArredondado = arredondarNumero($numero);
 
-        echo "Raiz Quadrada é: $Result_Raiz";
+        echo "Número arredondado: $numeroArredondado";
       }
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
